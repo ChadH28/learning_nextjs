8ce34cf0972063288843c92927ffdc36563ef0be
ChadH28 <chadhoosain@gmail.com>
2022-05-04 17:06:19 +0200

    minor change to itemPage

--- components/Item.js
+++ components/Item.js
@@ -4,7 +4,7 @@ import styles from '../styles/components/Item.module.css'
 export default function Item({ article }) {
   return (
     // getting page path 
-    <Link href='/item/[id]' as={`/item/${article.id}`}>
+    <Link href='/items/[id]' as={`/items/${article.id}`}>
       <a className={styles.card}>
         <h2>{article.title}</h2>
         <p>{article.body}</p>
