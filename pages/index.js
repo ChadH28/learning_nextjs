2f33940f6bdcdcef2067c2f35e8faab89fef8b7a
ChadH28 <chadhoosain@gmail.com>
2022-05-04 14:13:47 +0200

    worked on data fetching

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,8 @@ import Head from 'next/head'
 // import Image from 'next/image'
 // import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({articles}) {
+  console.log('articles: ', articles)
   return (
     <div>
       <Head>
@@ -15,4 +16,19 @@ export default function Home() {
       <h1>Highlights</h1>
     </div>
   )
+}
+
+
+export const getStaticProps = async () => {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=6`
+  )
+
+  const articles = await response.json()
+
+  return {
+    props: {
+      articles
+    }
+  }
 }
