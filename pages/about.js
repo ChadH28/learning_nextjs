11cc05c8a236a0bd15c0f3f9dc56203347faefd7
ChadH28 <chadhoosain@gmail.com>
2022-05-04 12:57:18 +0200

    worked on pages and how the routing takes place to different pages

---
+++ pages/about.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function About() {
+  return (
+    <div>
+      <Head>
+        <title>About</title>
+        <meta
+          name='keywords'
+          content='about page'
+        />
+      </Head>
+      <h1>about</h1>
+    </div>
+  )
+}
